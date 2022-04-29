2f4b243cb8fb39317e94a45162d63baa5779fad0
sergii-onyx <twilight.cympak@gmail.com>
2022-04-29 16:34:19 -0400

    connected server to database

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.get('/', (req, res) => {
     res.json({
